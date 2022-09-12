cf619dca401b3b3fe96245f2321e7e07511a5f8b
Deanna Lingum <lingumd@gmail.com>
2022-09-11 22:08:01 -0400

    Updating files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Bacteria_Biodiversity
+Create and deploy a biodiversity dashboard of belly button bacteria data on an interactive webpage using plotly, javascript, css, d3, html, and bootstrap.
+
+### [Bacteria Biodiversity Webpage](https://lingumd.github.io/Bacteria_Biodiversity/)
--- static/js/charts.js
+++ static/js/charts.js
@@ -56,33 +56,58 @@ function buildMetadata(sample) {
 
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
+
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("./static/data/samples.json").then((data) => {
+
     // 3. Create a variable that holds the samples array. 
+    var samples = data.samples;
+    // console.log(samples);
 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    // console.log(resultArray);
 
     //  5. Create a variable that holds the first sample in the array.
-
+    var result = resultArray[0];
+    // console.log(result);
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+    
+    // console.log(otu_ids);
+    // console.log(otu_labels);
+    // console.log(sample_values);
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    // var yticks = 
+    var yticks = otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
+    console.log(yticks);
+
+    var xticks = sample_values.slice(0,10).reverse();
 
     // 8. Create the trace for the bar chart. 
     var barData = [
+      { x: xticks,
+        y: yticks,
+        text: otu_labels,
+        type: "bar",
+        orientation: "h"
+      }
       
     ];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
+      title: `Top 10 Bacterial Species Found in Sample ${sample}`
      
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar", barData, barLayout);
+
+
   });
 }
